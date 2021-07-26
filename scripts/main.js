3d900d4be2d9ab00e6a4f3fccc4fcf3b8e7af319
micheaol <micheaol80@gmail.com>
2021-07-26 11:22:22 +0000

    Grid creation adjusted

--- scripts/main.js
+++ scripts/main.js
@@ -2,67 +2,26 @@
 const mainGrid = document.getElementById('main-grid');
 const container = document.getElementById('main-container');
 
+//function to create grids
+function createGrid(gridNumber) {
+    let gridArea = gridNumber * gridNumber;
+    for (let i = 0; i <= gridArea; i++) {
+        let gridItem = document.createElement('div');
+        gridItem.className = "grid-element";
+        gridItem.style.backgroundColor = 'yellow'; //For a test..........
+        mainGrid.style.gridTemplateColumns = `repeat(${gridNumber}, 1fr)`;
+        mainGrid.style.gridTemplateRows = `repeat(${gridNumber}, 1fr)`;
+        mainGrid.insertAdjacentElement('beforeend', gridItem);
 
+    }
+    var gridPixel = document.querySelectorAll('.grid-element');
+    gridPixel.forEach(gridPixel => gridPixel.addEventListener('mouseover', gridColor))
+        // console.log(gridPixel);
+}
 
-//function to make rows and column:
-function makeRows(rows, cols) {
-    mainGrid.style.setProperty("--grid-rows", rows);
-    mainGrid.style.setProperty("--grid-cols", cols);
-    for (let c = 0; c < (rows * cols); c++) {
-        let cell = document.createElement('div');
-        // cell.innerText = (c + 1);
-        mainGrid.appendChild(cell).className = "grid-element";
-    };
-};
+//create Grid Color function:
+function gridColor(params) {
 
-makeRows(16, 16);
-//Create other divs inside the grid-element
-// const firstDiv = document.createElement('div');
-// const secondDiv = document.createElement('div');
-// const thirdDiv = document.createElement('div');
-// const fourthDiv = document.createElement('div');
-// const fiftDiv = document.createElement('div');
-// const sixthDiv = document.createElement('div');
+}
 
-
-//Create class for the grid-element
-// firstDiv.className = "grid-element";
-// secondDiv.className = "grid-element";
-// thirdDiv.className = "grid-element";
-// fourthDiv.className = "grid-element";
-// fiftDiv.className = "grid-element";
-// sixthDiv.className = "grid-element";
-
-
-// //insert the div in the main grid
-// mainGrid.appendChild(firstDiv);
-
-
-// //insert the div before the previous divs
-// mainGrid.insertBefore(secondDiv, firstDiv);
-// mainGrid.insertBefore(thirdDiv, secondDiv);
-// mainGrid.insertBefore(fourthDiv, thirdDiv);
-// mainGrid.insertBefore(fiftDiv, fourthDiv);
-// mainGrid.insertBefore(sixthDiv, fiftDiv);
-
-
-// //Add event listener
-// firstDiv.addEventListener('mouseover', mouseEnter);
-// secondDiv.addEventListener('mouseenter', mouseEnterSec);
-// secondDiv.addEventListener('mouseout', mouseOut);
-
-// function mouseOut(e) {
-//     secondDiv.style.backgroundColor = 'rgb(15,55, 10)';
-// }
-
-// function mouseEnterSec(e) {
-//     secondDiv.style.backgroundColor = 'rgb(25,10, 255)';
-// }
-
-// function mouseEnter(e) {
-//     // console.log(e.type);
-//     firstDiv.style.backgroundColor = 'rgb(0, 191, 255)';
-
-// }
-
-// console.log(mainGrid.children);
+createGrid(10);
